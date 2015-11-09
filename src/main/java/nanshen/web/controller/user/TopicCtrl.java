cc8684d65d26f8246dc30739f717dfc0a0c32d43
Minghao Wang <wangmh@rd.netease.com>
2015-11-09 23:58:51 +0800

    MOD: 增加专题页

---
+++ src/main/java/nanshen/web/controller/user/TopicCtrl.java
@@ -0,0 +1,23 @@
+package nanshen.web.controller.user;
+
+import nanshen.data.PageType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/topic")
+public class TopicCtrl extends BaseCtrl {
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ModelAndView topicList(ModelMap model,
+								   @RequestParam(defaultValue = "1", required = true) int lookId) {
+		prepareHeader(model, PageType.TOPIC);
+		prepareHelloMsg(model);
+		return new ModelAndView("user/topicList");
+	}
+
+}
